243620f2f4449db235ebb298fe0cbec4280fe96b
Hector Javier Moreno <cytelsystem@gmail.com>
2023-02-28 14:53:02 -0500

    dsada

--- src/main/java/com/dh/microservicioInicial/controlador/MicroservicioInicialController.java
+++ src/main/java/com/dh/microservicioInicial/controlador/MicroservicioInicialController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping("/")
 public class MicroservicioInicialController {
@@ -27,6 +26,5 @@ public class MicroservicioInicialController {
     public String getSaludar() {
         return "Hola como estas";
     }
-
 
 }
--- src/main/java/com/dh/microservicioInicial/service/microservicioInicialService.java
+++ src/main/java/com/dh/microservicioInicial/service/microservicioInicialService.java
@@ -5,11 +5,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class microservicioInicialService {
 
-
     private String nombreApellido;
     public String getNombres(String nombre, String apellido) {
-
-
 
         return "Hola Como estas :" + nombre + " " + apellido;
     }
