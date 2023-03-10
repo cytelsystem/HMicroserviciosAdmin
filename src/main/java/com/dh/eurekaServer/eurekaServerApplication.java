0398e4a393bb27d7ea1b9cbc06ee4f24669850ae
Hector Javier Moreno <cytelsystem@gmail.com>
2023-03-10 13:11:55 -0500

    cambios en el pom

--- src/main/java/com/dh/eurekaServer/eurekaServerApplication.java
+++ src/main/java/com/dh/eurekaServer/eurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.dh.eurekaserver;
+package com.dh.eurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class eurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(eurekaServerApplication.class, args);
 	}
 
 }
--- src/test/java/com/dh/eurekaServer/eurekaServerApplicationTests.java
+++ src/test/java/com/dh/eurekaServer/eurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dh.eurekaserver;
+package com.dh.eurekaServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class eurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
