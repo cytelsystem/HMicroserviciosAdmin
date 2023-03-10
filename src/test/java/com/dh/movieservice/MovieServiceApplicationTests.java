49792100bb4fca269f7beb37e64f287d49438bb4
Hector Javier Moreno <cytelsystem@gmail.com>
2023-03-10 16:03:58 -0500

    cambio de rama a nueva de movieservice

--- src/test/java/com/dh/movieservice/MovieServiceApplicationTests.java
+++ src/test/java/com/dh/movieservice/MovieServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dh.configserver;
+package com.dh.movieservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class configserverApplicationTests {
+class MovieServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
