5afe473484b731fcb7a6e7902e838669d12dc5b5
Hector Javier Moreno <cytelsystem@gmail.com>
2023-03-10 19:41:13 -0500

    cambios en aplication

--- src/main/java/com/dh/movieservice/MovieServiceApplication.java
+++ src/main/java/com/dh/movieservice/MovieServiceApplication.java
@@ -2,8 +2,10 @@ package com.dh.movieservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MovieServiceApplication {
 
 	public static void main(String[] args) {
