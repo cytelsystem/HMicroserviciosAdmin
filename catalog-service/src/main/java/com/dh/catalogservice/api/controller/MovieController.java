29dcdbe4bd692a05978a56d64b1b8b4a18edde6f
Hector Javier Moreno <cytelsystem@gmail.com>
2023-04-06 16:02:41 -0500

    funcionando Circuitbreacker en controller con movie

--- catalog-service/src/main/java/com/dh/catalogservice/api/controller/MovieController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/api/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.dh.catalogservice.api.controller;
 
 import com.dh.catalogservice.api.client.IMovieService;
 import com.dh.catalogservice.domain.model.Movie;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,15 +21,14 @@ public class MovieController {
 
     @Autowired
     private IMovieService MovieService;
-
-    @CircuitBreaker(name = "movieCB", fallbackMethod = "fallback")
+    @CircuitBreaker(name="movieCB",fallbackMethod = "fallback")
     @GetMapping("/{genre}")
     public ResponseEntity<List<Movie>> getMovieByGenre(@PathVariable String genre) {
           return ResponseEntity.ok().body(MovieService.getMovieByGenre(genre));
     }
 
-    private ResponseEntity<List<Movie>> fallback(@PathVariable String genre) {
-          return new ResponseEntity("Movies " + genre + "no existe", HttpStatus.OK);
+    private ResponseEntity<List<Movie>> fallback(@PathVariable String genre, RuntimeException e) {
+          return new ResponseEntity("Base de datos no funciona", HttpStatus.OK);
     }
 
 
