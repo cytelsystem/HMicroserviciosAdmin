8f580a2959f1f2510ffe959fe136cac0c6b62eb3
Hector Javier Moreno <cytelsystem@gmail.com>
2023-04-06 14:14:44 -0500

    dsadsa

--- catalog-service/src/main/java/com/dh/catalogservice/api/controller/MovieController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/api/controller/MovieController.java
@@ -2,7 +2,10 @@ package com.dh.catalogservice.api.controller;
 
 import com.dh.catalogservice.api.client.IMovieService;
 import com.dh.catalogservice.domain.model.Movie;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,8 +22,15 @@ public class MovieController {
     private IMovieService MovieService;
 
     @GetMapping("/{genre}")
+
+    @CircuitBreaker(name="subscription",fallbackMethod = "subsciptionFallbackMethod")
+    @Retry(name = "subscription")
     ResponseEntity<List<Movie>> getMovieByGenre(@PathVariable String genre) {
           return ResponseEntity.ok().body(MovieService.getMovieByGenre(genre));
+    }
+
+    private ResponseEntity<List<Movie>> subsciptionFallbackMethod(@PathVariable String genre) {
+          return new ResponseEntity("Movies " + genre + "no existe", HttpStatus.OK);
     }
 
 
